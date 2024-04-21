645d9eaea5e5f816f9dced6f7f6db652c7a145a3
arihant-jain0 <jainari1208@gmail.com>
2024-04-21 19:16:08 +0530

    add:Api Error

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message="something Went Wrong",
+        errors=[],
+        statck= ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success =false
+        this.errors =errors
+
+        if(statck){
+            this.stack = statck
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+
+        }
+
+    }  
+}
+
+export default ApiError
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusode,data,message="Success"){
+         this.statusode =statusode
+         this.data =data
+         this.message= message
+         this.success = statusode <400
+    }
+}
