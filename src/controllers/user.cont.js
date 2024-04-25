4d56a25e06d39098cd6ed4130f8b519ff42a6196
arihant-jain0 <jainari1208@gmail.com>
2024-04-25 19:38:41 +0530

    Refactor user registration logic and add file upload functionality

--- src/controllers/user.cont.js
+++ src/controllers/user.cont.js
@@ -1,8 +1,9 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import { User } from "../models/user.js";
-import {uploadonCloudnary} from "../utils/cloudinary.js";
-
+import uploadonCloudnary from "../utils/cloudinary.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import { response } from "express";
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, username, email, password } = req.body;
 
@@ -22,18 +23,46 @@ const registerUser = asyncHandler(async (req, res) => {
   const ExistedUser = User.findOne({
     $or: [{ username }, { email }],
   });
-  console.log(ExistedUser);
+  // console.log(ExistedUser);
 
   if (ExistedUser) {
     throw new ApiError(400, "User already exists");
   }
   console.log(req.files?.avatar[0]?.path);
-  const avatar = req.files?.avatar[0]?.path;
-  const coverIamage = req.files?.coverImage[0]?.path;
+  const avatarLocal = req.files?.avatar[0]?.path;
+  const coverIamageLocal = req.files?.coverImage[0]?.path;
 
-  if (!avatar) {
+  if (!avatarLocal) {
     throw new ApiError(400, "avatar is required");
   }
+
+  const avatar = await uploadonCloudnary(avatarLocal);
+  const coverImage = await uploadonCloudnary(coverIamageLocal);
+  if (!avatar) {
+    throw new ApiError(400, "avatar File upload failed");
+  }
+
+  const user = await User.create({
+    fullname,
+    username: username.toLowerCase(),
+    email,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const registerUser = await User.findById(user._id).select(
+    "-password,-refreshToken"
+  );
+  console.log(registerUser);
+
+  if (!registerUser) {
+    throw new ApiError(500, "Error in Register a User");
+  }
+
+  return response
+    .status(201)
+    .json(new ApiResponse(200, registerUser, "User Registered Successfully"));
 });
 
 export default registerUser;
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -6,3 +6,5 @@ class ApiResponse {
          this.success = statusode < 400
     }
 }
+
+export default ApiResponse
